0f8d1bb87274f5cd599e3cfd7eefd2ba3a30dca9
Pranali Pandhare <136052843+pranalipandhare@users.noreply.github.com>
2023-10-23 23:53:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+window.addEventListener("scroll", function () {
+  const header = this.document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+//---------Toggle Menu
+function toggleMenu() {
+  const menuBar = document.querySelector(".menuToggle");
+  const nav = document.querySelector(".nav");
+  menuBar.classList.toggle("active");
+  nav.classList.toggle("active");
+}
+//---------Menu filter
+
+{
+  const menuFilter = document.querySelector(".menu-tabs"),
+    menuItems = document.querySelectorAll(".item");
+
+  // Fliter menu items
+  menuFilter.addEventListener("click", (event) => {
+    if (
+      event.target.classList.contains("tab-item") &&
+      !event.target.classList.contains("active")
+    ) {
+      //deactive existing active 'item
+      menuFilter.querySelector(".active").classList.remove("active");
+
+      //Active new 'Item
+      event.target.classList.add("active");
+
+      const target = event.target.getAttribute("data-target");
+
+      menuItems.forEach((item) => {
+        if (target === item.getAttribute("data-category") || target === "all") {
+          item.classList.remove("hide");
+          item.classList.add("show");
+        } else {
+          item.classList.add("hide");
+          item.classList.remove("show");
+        }
+      });
+    }
+  });
+}
+
+//---------Page loader
+window.addEventListener("load", function () {
+  //-------- loader
+
+  document.querySelector(".page-loader").classList.add("fade-out");
+  this.setTimeout(function () {
+    document.querySelector(".page-loader").style.display = "none";
+  });
+
+  //--------AOS or scrool after loader
+
+  AOS.init({
+    Delay: 500,
+    duration: 600,
+  });
+});
